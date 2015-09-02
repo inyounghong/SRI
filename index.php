21a3c39660934f83a992af70da64adf846f77457
Inyoung Hong <ih235@cornell.edu>
2015-09-01 22:06:11 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="SRI: The System of Rice Intensification">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="./images/icon.png" sizes="128x128">
+
+    <title>The System of Rice Intensification</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" media="screen">
+    
+    <!-- Javascript -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.is"></script>
+    
+</head>
+<body>
+
+	<?php include './includes/navigation.html' ?>
+
+	<div class="container">
+		Slidershow
+
+		<div class="section">
+			<h1>About SRI</h1>
+		</div>
+
+		<div class="section">
+			<h1>Global Impact</h1>
+		</div>
+
+		<div class="section">
+			<h1>About Us</h1>
+		</div>
+
+		<div class="section">
+			<h1>Projects</h1>
+		</div>
+	</div>
+
+</body>
+</html>
